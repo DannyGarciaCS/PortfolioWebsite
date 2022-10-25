6b0fb948a86a1a0b2b01719d46d2014ca71a5461
Danny Garcia <dannygarciacs@gmail.com>
2022-10-25 18:43:14 -0400

    Wireframe plane mesh generated

--- js/js_three.js
+++ js/js_three.js
@@ -1,18 +1,64 @@
 
 // Imports Three.js
 import * as THREE from "https://unpkg.com/three@0.145.0/build/three.module.js";
-
+ 
 // Waits for css to load
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Initialize scene objects
-    const scene = new THREE.Scene();
-    const earthElement = document.getElementById("threejs_element");
-    const camera = new THREE.PerspectiveCamera(75, earthElement.offsetWidth / earthElement.offsetHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById("threejs") });
+    const loader = new THREE.TextureLoader();
+    const displacementMap = loader.load("images/displacementMap.png")
+    const alphaMap = loader.load("images/alphaMap.png")
 
+    // Debugging GUI
+    const gui = new dat.GUI();
+
+    // Scene
+    const scene = new THREE.Scene();
+    const threeElement = document.getElementById("three_element");
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(75, threeElement.offsetWidth / threeElement.offsetHeight, 0.1, 1000);
+    camera.position.x = 0;
+    camera.position.y = 0;
+    camera.position.z = 3;
+    
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById("three") });
+    renderer.setSize(threeElement.offsetWidth, threeElement.offsetHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setClearColor(0xffffff, 0);
+
+     // Objects
+     const material = new THREE.MeshStandardMaterial({color: "gray", displacementMap, displacementScale: 0.35, alphaMap, transparent: true, wireframe: true, depthTest: false});
+     const plane = new THREE.Mesh(new THREE.PlaneGeometry(3, 3, 64, 64), material);
+     scene.add(plane);
+
+    // Lights
+    const pointLight = new THREE.PointLight(0xffffff, 2);
+    pointLight.position.x = 2;
+    pointLight.position.y = 3;
+    pointLight.position.z = 4;
+    scene.add(pointLight);
+ 
+     plane.rotation.x = 5;
+     gui.add(plane.rotation, "x").min(0).max(10);
+     gui.add(pointLight, "intensity").min(0).max(3);
+     gui.add(pointLight.position, "x").min(-30).max(30);
+     gui.add(pointLight.position, "y").min(-5).max(5);
+     gui.add(pointLight.position, "z").min(-10).max(100);
+
+     const col = { color: "#ffffff" }
+     gui.addColor(col, "color").onChange(() => {
+        pointLight.color.set(col.color);
+     })
+
+     const clock = new THREE.Clock()
+     
     // Main renderer loop
     function animate() {
+
+        const elapsedTime = clock.getElapsedTime();
+        plane.rotation.z = 0.25 * elapsedTime;
 
         // Natural rotation
         requestAnimationFrame(animate);
