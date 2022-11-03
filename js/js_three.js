e19bcf994f2e03aaf52174f20eb5471ffedb29b9
Danny Garcia <dannygarciacs@gmail.com>
2022-11-03 19:59:47 -0400

    Injected 3D scene into page

--- js/js_three.js
+++ js/js_three.js
@@ -7,13 +7,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Loads used textures
     const loader = new THREE.TextureLoader();    
-    const map = loader.load("images/map.png")
-    const displacementMap = loader.load("images/displacementMap.png")
+    const displacementMap = loader.load("images/displacementMap2.png")
     const alphaMap = loader.load("images/alphaMap.png")
 
     // Scene
     const scene = new THREE.Scene();
-    const threeElement = document.getElementById("three_element");
+    const threeElement = document.getElementById("canvas_element");
 
     // Camera
     const camera = new THREE.PerspectiveCamera(75, threeElement.offsetWidth / threeElement.offsetHeight, 0.1, 1000);
@@ -22,22 +21,15 @@ document.addEventListener("DOMContentLoaded", () => {
     camera.position.z = 3;
     
     // Renderer
-    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById("three") });
+    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById("element") });
     renderer.setSize(threeElement.offsetWidth, threeElement.offsetHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setClearColor(0xffffff, 0);
 
-     // Main plane
-     const mMaterial = new THREE.MeshStandardMaterial({
-        color: "gray", displacementMap, displacementScale: 0.5, map, alphaMap, transparent: true, depthTest: false, opacity: 0.75
-    }); const mPlane = new THREE.Mesh(new THREE.PlaneGeometry(3, 3, 64, 64), mMaterial);
-    mPlane.rotation.x = 5;
-    scene.add(mPlane);
-
      // Wireframe plane
      const wMaterial = new THREE.MeshStandardMaterial({
         color: "white", displacementMap, displacementScale: 0.5, alphaMap, transparent: true, wireframe: true, depthTest: false, opacity: 0.25
-    }); const wPlane = new THREE.Mesh(new THREE.PlaneGeometry(3, 3, 64, 64), wMaterial);
+    }); const wPlane = new THREE.Mesh(new THREE.PlaneGeometry(3, 3, 128, 128), wMaterial);
     wPlane.rotation.x = 5;
     scene.add(wPlane);
 
@@ -47,6 +39,12 @@ document.addEventListener("DOMContentLoaded", () => {
     pointLight.position.y = 2;
     pointLight.position.z = 6.5;
     scene.add(pointLight);
+
+    let trueDisplacement = 0.15;
+    let direction = 0;
+    let displacementMax = 0.25;
+    let displacementMin = -0.25;
+    let displacementRate = 0.0005;
 
     // Event handling
     document.addEventListener("mousemove", animateTerrain);
@@ -59,15 +57,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
         // Rotates env
         const elapsedTime = clock.getElapsedTime();
-        mPlane.rotation.z = 0.25 * elapsedTime;
         wPlane.rotation.z = 0.25 * elapsedTime;
-
-        mPlane.material.displacementScale = 0.4 + mouseY / 4500
-        wPlane.material.displacementScale = 0.4 + mouseY / 4500
+        wPlane.material.displacementScale = trueDisplacement + 0.4 + mouseY / 4500;
 
         // REnders frame
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
+
+        if(direction == 0) {
+            trueDisplacement -= displacementRate;
+            if(trueDisplacement <= displacementMin) {
+                direction = 1;
+            }
+        } else if(direction == 1) {
+            trueDisplacement += displacementRate;
+            if(trueDisplacement >= displacementMax) {
+                direction = 0;
+            }
+        }
 
     } animate()
 
