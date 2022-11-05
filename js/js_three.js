0d18220e00ad13337117f40d9e44881add38c0f7
Danny Garcia <dannygarciacs@gmail.com>
2022-11-05 17:53:29 -0400

    Reformatted JavaScript and intro columns

--- js/js_three.js
+++ js/js_three.js
@@ -1,12 +1,10 @@
-let house;
-var mixer;
-const gui = new dat.GUI({autoPlace: false})
-gui.domElement.id = 'gui';
-var customContainer = document.getElementById('my-gui-container');
-customContainer.appendChild(gui.domElement);
 
 // Waits for css to load
 document.addEventListener("DOMContentLoaded", () => {
+
+    // 3D variables initialization
+    let chocobo;
+    var mixer;
 
     // Loads used textures
     const loader = new THREE.TextureLoader();
@@ -19,10 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
     // Camera
     const camera = new THREE.PerspectiveCamera(75, threeElement.offsetWidth / threeElement.offsetHeight, 0.1, 1000);
     camera.position.set(0, 0.5, 2.2);
-
-    gui.add(camera.position, "x").min(-5).max(5);
-    gui.add(camera.position, "y").min(-5).max(5);
-    gui.add(camera.position, "z").min(-5).max(5);
     
     // Renderer
     const renderer = new THREE.WebGLRenderer({ antialias: true, canvas: document.getElementById("element"), alpha:true });
@@ -30,20 +24,22 @@ document.addEventListener("DOMContentLoaded", () => {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setClearColor(0xffffff, 0);
 
-
-
+    // Loads GLTF scene
     const gltfLoader = new THREE.GLTFLoader();
     gltfLoader.load("model/scene.gltf", function(gltf){
 
+        // Gets chocobo into the scene
         scene.add(gltf.scene);
-        house = gltf.scene.children[0];
-        house.rotation.z = 2
+        chocobo = gltf.scene.children[0];
+        chocobo.rotation.z = 2
 
-        mixer= new THREE.AnimationMixer(gltf.scene);
+        // Animates chocobo
+        mixer = new THREE.AnimationMixer(gltf.scene);
         gltf.animations.forEach((clip) => {
-        mixer.clipAction(clip).play(); });
-        scene.add(gltf.scene);
+            mixer.clipAction(clip).play();
+        });
 
+        // Executes main loop after model loaded
         animate();
     });
 
@@ -51,21 +47,10 @@ document.addEventListener("DOMContentLoaded", () => {
     map, transparent: true
     }); const backdrop = new THREE.Mesh(new THREE.PlaneGeometry(3, 3, 1, 1), backdropMaterial);
     let backdropY = 1.0;
-    let backdropX = 0.0;
     backdrop.rotation.x = 6.3;
     backdrop.position.y = backdropY;
     backdrop.position.z = -1;
     scene.add(backdrop);
-    gui.add(backdrop.position, "x").min(0).max(10);
-    gui.add(backdrop.position, "y").min(0).max(10);
-    gui.add(backdrop.position, "z").min(-10).max(0);
-
-    // Lights
-    const ambientLight = new THREE.AmbientLight(0x404040, 5);
-    scene.add(ambientLight);
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 5);
-    directionalLight.position.set(10, 10, 10);
-    scene.add(directionalLight);
 
     // Event handling
     document.addEventListener("mousemove", animateTerrain);
@@ -79,9 +64,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // Main renderer loop
     function animate() {
 
-        // Rotates env
+        // Moves backdrop based on mouse position
         let xTarget = 0;
-        if(mouseX != 0) { xTarget = (-mouseX + 960) / 6000 ; }
+        if(mouseX != 0) { xTarget = (-mouseX + (screen.width / 2)) / 6000 ; }
         else { xTarget = 0; }
         gsap.to(backdrop.position, {
             x: xTarget,
@@ -89,21 +74,22 @@ document.addEventListener("DOMContentLoaded", () => {
             duration: 1
         })
         
-        house.rotation.z -= 0.0005;
+        // Rotates chocobo
+        chocobo.rotation.z -= 0.0005;
 
-        gsap.to(house.position, {
+        // Moves chocobo based on mouse position
+        gsap.to(chocobo.position, {
             x: -xTarget,
             y: -mouseY / 6000,
             duration: 1
         })
 
+        // Moves chocobo's animation
+        mixer.update(0.01);
 
-        mixer.update( 0.01 )
-
-        // REnders frame
+        // Renders frame
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
 
     }
-
 });
