8dc21687e954f879a7e78558d8378f02d05647ac
Danny Garcia <dannygarciacs@gmail.com>
2022-12-06 19:10:47 -0500

    Responsive nav and header added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+/*
 // Removes selected class from tabs that have it
 function resetSelection(home, about, work, contact) {
     if(home.classList.contains("selected")) { home.classList.remove("selected"); }
@@ -55,3 +55,4 @@ document.addEventListener("DOMContentLoaded", (event) => {
         pickClass(home, about, work, contact);
     });
 })
+*/
